86107a0316293b5d6da51cf23e697b58e42e17c6
wangm <wangm@txxy.com>
2022-05-31 16:01:18 +0800

    spring-advanced-boot: controller & banner

---
+++ spring-advanced-boot/README.md
@@ -0,0 +1,4 @@
+# spring-advanced-boot
+
+## 资料
+* [自定义控制台输出的springboot字样的banner文字](http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20)
---
+++ spring-advanced-boot/src/main/java/org/noneorone/sab/controller/Application.java
@@ -0,0 +1,11 @@
+package org.noneorone.sab.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
