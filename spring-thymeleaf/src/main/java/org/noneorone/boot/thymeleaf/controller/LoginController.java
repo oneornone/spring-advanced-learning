c9d788014128c3234927bbb8dd897e8a8da599c6
wangm <wangm@txxy.com>
2022-06-13 18:04:29 +0800

    spring-thymeleaf: 测试login页面中使用ajax的方式来调用接口的示例

--- spring-thymeleaf/src/main/java/org/noneorone/boot/thymeleaf/controller/LoginController.java
+++ spring-thymeleaf/src/main/java/org/noneorone/boot/thymeleaf/controller/LoginController.java
@@ -3,6 +3,7 @@ package org.noneorone.boot.thymeleaf.controller;
 import org.noneorone.boot.thymeleaf.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,14 +12,13 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
 
     @PostMapping("/login/ajax")
-    public String loginAjax(@RequestParam(value = "account1") String account, @RequestParam(value = "pwd1") String pwd, HttpServletRequest request, HttpSession session) {
+    public ModelAndView loginAjax(@RequestParam(value = "account1") String account, @RequestParam(value = "pwd1") String pwd, HttpServletRequest request, HttpSession session) {
         User user = new User(account, pwd);
+        ModelAndView mv = new ModelAndView("redirect:/main", "user", user);
         if (user.isGuest()) {
             user.setSid(session.getId());
-            request.setAttribute("user", user);
-            return "forward:/main";
         }
-        return null;
+        return mv;
     }
 
     @RequestMapping(value = "/login/form", method = RequestMethod.POST)
