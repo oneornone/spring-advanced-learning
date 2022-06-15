842d332a61d0d0afb944fb4f08a148cb3b1ba776
wangm <wangm@txxy.com>
2022-06-15 16:52:22 +0800

    spring-thymeleaf: 测试controller中exception

---
+++ spring-thymeleaf/src/main/java/org/noneorone/boot/thymeleaf/controller/ExceptionController.java
@@ -0,0 +1,31 @@
+package org.noneorone.boot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exception")
+public class ExceptionController {
+
+    @RequestMapping("/index")
+    public static String index() {
+        return "exception";
+    }
+
+    @RequestMapping("/hello")
+    public static String hello() throws Exception{
+        throw new Exception("→Hello Exception :-D");
+    }
+
+    @RequestMapping("/test")
+    public static String test() throws Exception{
+        System.out.println("test exception ...");
+        int a = 5;
+        int b = 0;
+        int c = a/b;
+        return "success";
+    }
+
+
+
+}
